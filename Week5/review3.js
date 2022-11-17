1be33a7077114f1442c1e722a50dd1cf1d27d703
fahlay <116155807+fahlay@users.noreply.github.com>
2022-11-17 09:21:25 +0700

    Create review3.js

---
+++ Week5/review3.js
@@ -0,0 +1,8 @@
+let obj = {//คีย์ กับ เวลู่
+    "studentID" : 64309010047 ,    
+    "name"      : "Java" ,
+    "lastname"  : "Script" ,
+    "grade"     : 3.45 ,
+    "Tel"       : [191,1112]
+}
+console.log(obj.name, obj.grade, obj.Tel[0]) //เรียกใช้ตัวแปร obj
