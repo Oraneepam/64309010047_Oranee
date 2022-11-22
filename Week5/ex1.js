77d151cb6bdf85abad70a6e9ac666be11fb30c71
Oraneepam <116155816+Oraneepam@users.noreply.github.com>
2022-11-22 12:36:25 +0700

    Update ex1.js

--- Week5/ex1.js
+++ Week5/ex1.js
@@ -1,23 +1,30 @@
-let order = [{
+let order = [{ //สร้างตัวแปรเก็บข้อมูล เอาArryเข้ามาช่วยในการเก็บข้อมูล
     "Customer": "Mr.lee",
     "ItemOrder": ["A4 Paper", "Ruler"],
     "Qty": [5, 2],
     "Price": [120, 25]
-}, {
+}, 
+    { //ออฟเจ็กใหม่
     "Customer": "Mr.Yue",
     "ItemOrder": ["Flash Drive", "A4 Paper"],
     "Qty": [2, 3],
     "Price": [130, 120]
-}, {
+}, 
+    {
     "Customer": "Miss pam",
     "ItemOrder": ["Flash Drive", "VGA card", "SSD"],
     "Qty": [2, 3, 1],
     "Price": [130, 300, 250]
 }]
+
 for ( i = 0; i < order.length; i++) {
-    let sum = 0;
+    let sum = 0; //ประกาศsumเก็บค่าตัวแปรและผลรวม
+
+
+
     for (let j = 0; j < order[i].ItemOrder.length; j++) {
-        sum += order[i].Qty[j] * order[i].Price[j];
+        sum += order[i].Qty[j] * order[i].Price[j];  //เอามาคูณกันแล้ว +sum
     }
-    console.log(order[i].Customer, sum);
+
+    console.log(order[i].Customer, sum); //ดูว่าCustomerใครเป็นคนซื้อ *เข้าถึงออฟเจ็กโดยใช้ . 
 }
