efcbd1ddf6e2ebbc3ac97ece98820f9eda96a6fb
Oraneepam <116155816+Oraneepam@users.noreply.github.com>
2022-11-22 09:30:15 +0700

    Update ex1.js

--- Week5/ex1.js
+++ Week5/ex1.js
@@ -1,11 +1,23 @@
-let product ={
-    "A4paper":120,
-    "Ruler":25,
-    "FlashDrive":130
+let order = [{
+    "Customer": "Mr.lee",
+    "ItemOrder": ["A4 Paper", "Ruler"],
+    "Qty": [5, 2],
+    "Price": [120, 25]
+}, {
+    "Customer": "Mr.Yue",
+    "ItemOrder": ["Flash Drive", "A4 Paper"],
+    "Qty": [2, 3],
+    "Price": [130, 120]
+}, {
+    "Customer": "Miss Baitoey",
+    "ItemOrder": ["Flash Drive", "VGA card", "SSD"],
+    "Qty": [2, 3, 1],
+    "Price": [130, 300, 250]
+}]
+for ( i = 0; i < order.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < order[i].ItemOrder.length; j++) {
+        sum += order[i].Qty[j] * order[i].Price[j];
+    }
+    console.log(order[i].Customer, sum);
 }
-//Mr. Lee
-console.log("Mr. Lee",product.A4paper*5 +product.Ruler*2)
-//Mr. Yue
-console.log("Mr. Yue",product.FlashDrive*2 +product.A4paper*3)
-//Ms.Pam
-console.log("Ms.Pond ",product.FlashDrive*2 +product.A4paper*3+product.Ruler*2)
